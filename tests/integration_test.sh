9e95fb46da2cffd8c1f2ec2d052c5c3ae3730c69
Haili Hu <hailihu@gmail.com>
2025-01-28 16:22:06 +0100

    tag v2.3

--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -5,7 +5,7 @@
 # Check if version is as expected
 test_ada_version() {
     result=`ada/ada --version`
-    assertEquals "Check ada version:" "v2.2" ${result}
+    assertEquals "Check ada version:" "v2.3" ${result}
 }
 
 # Create directory on dCache
